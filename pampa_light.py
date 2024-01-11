79b5cdeb28512deaeff5b41ab94318a2beb81014
touzet <helene.touzet@gmail.com>
2024-01-11 13:46:53 +0100

    update of the options

--- pampa_light.py
+++ pampa_light.py
@@ -21,22 +21,16 @@ def main():
     group2.add_argument("-o", dest="output", help="Output path (should include the output file name)", type=str, required=True)
 
     group = parser.add_argument_group('preset options for organism selection')
-    group.add_argument("--mammals", help="Preset option: use pre-computed peptide table and taxonomy for mammals.", action='store_true')
+    group.add_argument("--mammals", help="use pre-computed peptide table and taxonomy for mammals.", action='store_true')
 
-    group3=parser.add_argument_group('\nnon-mandatory advanced options: organism and markers selection')
-    group3.add_argument("-p", dest="peptide_table", help="Peptide table (TSV file)", type=str)
-    group3.add_argument("-f", dest="fasta", help="Fasta sequences", type=str)
-    group3.add_argument("-d", dest="directory",  help="Directory where to find  Fasta files.", type=str)
-    group3.add_argument("-l", dest="limit",  help="Limit the set of peptides or fasta sequences to organisms, molecules or sequence ID specified in this file  (TSV files)", type=str, required=False)
-    group3.add_argument("-t", dest="taxonomy", help="Taxonomy (TSV file), optional.", type=str)
-
-    group4=parser.add_argument_group('\nnon-mandatory advanced options: suboptimal solutions')
-    group4.add_argument("-n", dest="neighbour", help="Also provide near-optimal solutions within a specified percentage margin, ranging between 0 and 100. Default is 100. Only suboptimal solutions are provided.", type=int, required=False, default=100)
-    group4.add_argument("-a", dest="all", action='store_true', help="Provide all solutions, and not only suboptimal solutions, within the percentage margin specified with parameter -n. Default is False.", required=False)  
+    group3=parser.add_argument_group('\nnon-mandatory advanced options')
+    group3.add_argument("-l", dest="limit",  help="Limit the set of markers to organisms, molecules or sequence ID specified in this file  (TXT files)", type=str, required=False)
+    group3.add_argument("-n", dest="neighbour", help="Also provide near-optimal solutions within a specified percentage margin, ranging between 0 and 100. Default is 100. Only suboptimal solutions are provided.", type=int, required=False, default=100)
+    group3.add_argument("-a", dest="all", action='store_true', help="Provide all solutions, and not only suboptimal solutions, within the percentage margin specified with parameter -n. Default is False.", required=False)  
   
     args = parser.parse_args()
 
-    main_assign.main(args.spectra, args.taxonomy, args.peptide_table, args.fasta, args.directory, args.limit, args.error, args.neighbour, args.all, args.output, args.mammals, True)  
+    main_assign.main(args.spectra, None, None, None, None, args.limit, args.error, args.neighbour, args.all, args.output, args.mammals, True)  
     
 if __name__ == "__main__":
     main()
