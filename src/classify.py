85b61f02b1b3b251d64ce26c3a7b8548d735800e
touzet <helene.touzet@gmail.com>
2025-04-23 13:51:04 +0200

    new organization with params_checker

--- src/classify.py
+++ src/classify.py
@@ -17,104 +17,9 @@ from src import fasta_parsing as fa
 from src import compute_masses
 from src import message
 from src import limit as lmt
+from src import config
+from src import params_checker
 
-
-def check_and_update_parameters(spectra, taxonomy, peptide_table, fasta, directory, limit, deamidation, error, neighbour, all, output, mammals):
-    """
-    Parameters checking and fixing. Configuration of loggers
-    """
-
-    if output is None:
-        message.configure("")
-        message.escape("Missing parameter: output (-o).")
-        
-    output_dir, output_file = os.path.split(output)
-    
-    if len(output_dir)>0 :
-        # Ensure the output directory exists. If not, create it.
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-
-    message.configure(output_dir)
-            
-    if spectra is None:
-        message.escape("Missing parameter: spectra (-s).")
-    if not os.path.isdir(spectra):    
-        message.escape("Directory "+spectra+" not found.")
-    if error is None:
-        message.escape("Missing parameter: error (-e)")
-    if error<0:
-        message.escape("Parameter error (-e) should be positive.")
-    if output is None:
-        message.escape("Missing parameter: output (-o)")
-
-    if mammals and (peptide_table or fasta or directory):
-        message.warning("Parameters -p, -f and -d are not compatible with --mammals. Applying --mammals parameter.")
-        peptide_table=None
-        fasta=None
-        directory=None
-
-    if mammals :
-        if not os.path.isfile("Taxonomy/taxonomy_mammals.tsv"):
-            message.escape("The taxonomy file has been deleted.")
-        if not os.path.isfile("Peptide_tables/table_mammals.tsv"):
-            message.escape("The peptide table file has been deleted.")
-        taxonomy="Taxonomy/taxonomy_mammals.tsv"
-        peptide_table=["Peptide_tables/table_mammals.tsv"]
-
-    if taxonomy:
-        if not os.path.isfile(taxonomy):
-            message.warning("File "+taxonomy+" not found. Ignored.")
-            taxonomy=None
-        elif  os.path.getsize(taxonomy) == 0:
-            message.warning("File "+taxonomy+" is empty. Ignored.")
-            taxonomy=None
-    if limit:
-        if not os.path.isfile(limit):
-            message.warning("File "+limit+" not found. No limit applied.")
-        elif os.path.getsize(limit) == 0:
-            message.warning("File "+limit+" is empty. No limit applied.")
-            
-    if neighbour not in range(101):
-        neighbour=100
-        message.warning("Parameter -n (neighbouring): value is 100")
-        
-    q = (peptide_table, fasta, directory)
-    if not (q[0] or q[1] or q[2]):
-        message.escape("Missing information for marker peptides (-p, -f or -d).")
-    if (q[0] and q[1]) or (q[0] and q[2]) or (q[1] and q[2]) :
-        message.escape("Options -p (peptide_table), -f (fasta) and -d (directory of fasta files) are mutually incompatible.")
-    if q[1]:
-        if not os.path.isfile(fasta):
-            message.escape("File "+fasta+" (-f) not found.")
-        elif os.path.getsize(fasta) == 0:
-            message.escape("File "+fasta+" is empty.")
-    if q[2]:
-        if not os.path.isdir(directory):
-            message.escape("Directory "+directory+" (-d) not found.")
-    if q[0]:
-        for pep in peptide_table:
-            if not os.path.isfile(pep):
-                message.escape("File "+pep+" not found.")
-            elif os.path.getsize(pep) == 0:
-                message.escape("File "+pep+" is empty.")
-    
-    extension=output_file[-4:].lower()
-    if extension==".tsv":
-        output_file=output_file[:-4]
-
-    output=os.path.join(output_dir, output_file)
-    output2=os.path.join(output_dir, "detail_"+output_file+".tsv")
-    report_file="report_"+output_file+".txt"
-    
-    if not peptide_table:
-        new_table=os.path.join(output_dir, "table_"+output_file)
-    else:
-        new_table=None
-
-    return (spectra, taxonomy, peptide_table, fasta, directory, limit, deamidation, error, neighbour, all, output, output2, output_dir, report_file, new_table)
-
-        
 def create_report(report_file, output_dir, spectra, final_list_of_spectra, taxonomy, taxonomy_tree, peptide_table, fasta, directory,  limit, deamidation, error, neighbour, all, output, output2, new_table, set_of_markers, WEB):
 
     # TO DO: display constraints
@@ -222,7 +127,7 @@ def main(spectra, taxonomy, peptide_table, fasta, directory, limit, deamidation,
    
     try:
 
-        (spectra, taxonomy, peptide_table, fasta, directory, limit, deamidation, error, neighbour, allsolutions, output, output2, output_dir, report_file, new_table)=check_and_update_parameters(spectra, taxonomy, peptide_table, fasta, directory, limit, deamidation, error, neighbour, allsolutions, output, mammals)
+        (spectra, taxonomy, peptide_table, fasta, directory, limit, deamidation, error, neighbour, allsolutions, output, output2, output_dir, report_file, new_table)=params_checker.check_and_update_parameters_classify(spectra, taxonomy, peptide_table, fasta, directory, limit, deamidation, error, neighbour, allsolutions, output, mammals)
 
 
         # parsing spectra files
@@ -257,19 +162,21 @@ def main(spectra, taxonomy, peptide_table, fasta, directory, limit, deamidation,
             set_of_markers=compute_masses.add_PTM_or_masses_to_markers(set_of_markers)
         if fasta or directory:
             set_of_sequences = fa.build_set_of_sequences(fasta, directory, list_of_constraints, primary_taxonomy)
+            config_digestion=config.config_digestion()
             if len(set_of_sequences)==0:
                  message.escape("Fasta file(s): No valid sequences found.\nPlease refer to the warning.log file to trace back the errors.")
-            set_of_markers = markers.sort_and_merge(compute_masses.add_PTM_or_masses_to_markers(seq.in_silico_digestion(set_of_sequences)))
+            set_of_markers=compute_masses.add_PTM_or_masses_to_markers(seq.in_silico_digestion(set_of_sequences,config_digestion))
         if len(set_of_markers)==0:
             message.escape("No valid peptide marker found.\nPlease refer to the warning.log file to trace back the errors.")
 
         if deamidation:
-            print("DEAMIDATION")
             set_of_codes=set()
             for constraint in list_of_constraints:
                 if 'Deamidation' in constraint:
                     set_of_codes.update(constraint['Deamidation'])
             set_of_markers=set_of_markers.union(compute_masses.add_deamidation(set_of_markers, set_of_codes))
+       
+        set_of_markers=markers.sort_and_merge(set_of_markers)
        
         # construction of the secondary taxonomy and suppression of taxid not present in the taxonomy
         set_of_taxid={m.taxid() for m in set_of_markers}
